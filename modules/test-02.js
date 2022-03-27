de386f343468e8b2d7070c80135fee1146f52da8
aboongm <aboongm@yahoo.com>
2022-03-27 11:38:08 +0530

    Add test and check of awesome books

--- modules/test-02.js
+++
@@ -1,7 +0,0 @@
-function Display(param = 0) {
-  console.log('This is the program called with parameter: ', param);
-
-  return 'Back from Display';
-}
-
-export default Display; //exporting it to be imported into another js file
